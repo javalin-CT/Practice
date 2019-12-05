124a199023daa3a3d22288be74471af223f047ba
javalin-CT <jcw.cybertek@gmail.com>
2019-12-05 17:41:56 -0500

    initial commit' Initial commit git clean

---
+++ src/test/java/com/Tests/practice.java
@@ -0,0 +1,13 @@
+package com.Tests;
+
+public abstract class practice {
+    public static void whatever(){
+
+    }
+
+    public void whatever2(){
+
+    }
+
+    public abstract void whatever4();
+}
