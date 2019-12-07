8ef9d771fc60adb16d82541c2a65f9839366b9f1
javalin-CT <55003393+javalin-CT@users.noreply.github.com>
2019-12-06 20:12:41 -0500

    Update practice.java

--- src/test/java/com/Tests/practice.java
+++ src/test/java/com/Tests/practice.java
@@ -3,6 +3,6 @@ package com.Tests;
 public class practice {
     public static void main(String[] args) {
         System.out.println("Hi world");
-
+        System.out.println("Hi world again not more world");
     }
 }
