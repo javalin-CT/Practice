056c602954c01ced2972f69e86e33b7d14a68720
javalin-CT <jcw.cybertek@gmail.com>
2019-12-06 19:37:19 -0500

    not sure

--- src/test/java/com/Tests/secondClass.java
+++ src/test/java/com/Tests/secondClass.java
@@ -1,4 +1,9 @@
 package com.Tests;
 
 public class secondClass {
+    public static void main(String[] args) {
+        String myname = "Jonathan";
+        System.out.println(myname);
+        System.out.println("this is another line");
+    }
 }
