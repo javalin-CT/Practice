50bd48db690b08dd40356659915021dd7c0d4ea5
javalin-CT <jcw.cybertek@gmail.com>
2019-12-12 10:44:31 -0500

    changed the develop branch

--- src/test/java/com/Tests/firstClass.java
+++ src/test/java/com/Tests/firstClass.java
@@ -1,6 +1,6 @@
 package com.Tests;
 
-public class practice {
+public class firstClass {
     public static void main(String[] args) {
         System.out.println("Hi world");
         System.out.println("Hi world again not more world");
--- src/test/java/com/Tests/secondClass.java
+++ src/test/java/com/Tests/secondClass.java
@@ -4,10 +4,7 @@ public class secondClass {
     public static void main(String[] args) {
         String myname = "Luis Batonn";
         System.out.println(myname);
-        System.out.println("this is another line");
-        System.out.println("changed in remote");
-        System.out.println("changed in remote...again");
-        System.out.println("changed in remote...again");
+        System.out.println("this is from the develop branchgit ch");
 
 
     }
